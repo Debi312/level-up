6c56a578fd557f09e62872ded45a8af354a2d690
Debi312 <deboragarciaescudero@gmail.com>
2024-09-04 22:53:44 +0200

    Integrate  populateDatabase function to run during the server initialization process. The database is populated with initial data upon connecting to MongoDB before the API starts listening for requests

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import mongoose from "mongoose"
 import routeHandler from "./handlers/index.js"
 import errorHandler from "./handlers/errorHandler.js"
+import { populateDatabase } from "./data/populate/populate.js"
 
 
 const { MONGODB_URL, PORT } = process.env
@@ -17,25 +18,24 @@ mongoose.connect(MONGODB_URL)
         api.use(cors())
 
         const jsonBodyParser = express.json({ strict: true, type: "application/json" })
-
-
-        //comprobamos que la api funciona
-        api.get("/", (req, res) => res.send("Hello world"))
-        api.post("/users", jsonBodyParser, routeHandler.registerUserHandler)
-        api.post("/users/auth", jsonBodyParser, routeHandler.authenticateUserHandler)
-        api.get("/users/:targetUserId", routeHandler.getUsernameHandler)
-        api.get("/workouts/:workoutType", routeHandler.getRandomWorkoutHandler)
-        api.post("/posts", jsonBodyParser, routeHandler.createPostHandler)
-        api.get("/posts", routeHandler.getPostsHandler)
-        api.patch("/posts/:postId/likes", routeHandler.toggleLikePostHandler)
-        api.post("/posts/:postId/comments", jsonBodyParser, routeHandler.createCommentHandler)
-        api.get("/posts/:postId/comments", routeHandler.getAllCommentsHandler)
-        api.get("/results", routeHandler.getAllResultsHandler)
-        api.get("/results/:resultId", routeHandler.getResultHandler)
-        api.delete("/results/:resultId", routeHandler.deleteResultHandler)
-        api.patch("/results/:resultId", jsonBodyParser, routeHandler.updateResultHandler)
-        api.use(errorHandler)
-        api.listen(PORT, () => console.log(`API running on PORT ${PORT}`))
-
+        return populateDatabase()
+            .then(() => {
+                api.get("/", (req, res) => res.send("Hello world"))
+                api.post("/users", jsonBodyParser, routeHandler.registerUserHandler)
+                api.post("/users/auth", jsonBodyParser, routeHandler.authenticateUserHandler)
+                api.get("/users/:targetUserId", routeHandler.getUsernameHandler)
+                api.get("/workouts/:workoutType", routeHandler.getRandomWorkoutHandler)
+                api.post("/posts", jsonBodyParser, routeHandler.createPostHandler)
+                api.get("/posts", routeHandler.getPostsHandler)
+                api.patch("/posts/:postId/likes", routeHandler.toggleLikePostHandler)
+                api.post("/posts/:postId/comments", jsonBodyParser, routeHandler.createCommentHandler)
+                api.get("/posts/:postId/comments", routeHandler.getAllCommentsHandler)
+                api.get("/results", routeHandler.getAllResultsHandler)
+                api.get("/results/:resultId", routeHandler.getResultHandler)
+                api.delete("/results/:resultId", routeHandler.deleteResultHandler)
+                api.patch("/results/:resultId", jsonBodyParser, routeHandler.updateResultHandler)
+                api.use(errorHandler)
+                api.listen(PORT, () => console.log(`API running on PORT ${PORT}`))
+            })
     })
     .catch(error => console.error(error))

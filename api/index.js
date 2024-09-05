f773ee5a16f18f65658215806b32d4c03bfd37d1
Debi312 <deboragarciaescudero@gmail.com>
2024-09-05 09:26:31 +0200

    Remove the redundant database connection logic from populateDatabase; execute populateDatabase() whithin 'api.listen' callback, ensuring the database is fully connected before populating

--- api/index.js
+++ api/index.js
@@ -4,38 +4,44 @@ import cors from "cors"
 import mongoose from "mongoose"
 import routeHandler from "./handlers/index.js"
 import errorHandler from "./handlers/errorHandler.js"
-import { populateDatabase } from "./data/populate/populate.js"
-
+import populateDatabase from "./data/populate/populate.js"
 
 const { MONGODB_URL, PORT } = process.env
 
+
 mongoose.connect(MONGODB_URL)
-    .then(() => {
+    .then( () => {
+        console.log("Database connected")
+
         const api = express()
-
         api.use(express.static("public"))
-
         api.use(cors())
-
         const jsonBodyParser = express.json({ strict: true, type: "application/json" })
-        return populateDatabase()
-            .then(() => {
-                api.get("/", (req, res) => res.send("Hello world"))
-                api.post("/users", jsonBodyParser, routeHandler.registerUserHandler)
-                api.post("/users/auth", jsonBodyParser, routeHandler.authenticateUserHandler)
-                api.get("/users/:targetUserId", routeHandler.getUsernameHandler)
-                api.get("/workouts/:workoutType", routeHandler.getRandomWorkoutHandler)
-                api.post("/posts", jsonBodyParser, routeHandler.createPostHandler)
-                api.get("/posts", routeHandler.getPostsHandler)
-                api.patch("/posts/:postId/likes", routeHandler.toggleLikePostHandler)
-                api.post("/posts/:postId/comments", jsonBodyParser, routeHandler.createCommentHandler)
-                api.get("/posts/:postId/comments", routeHandler.getAllCommentsHandler)
-                api.get("/results", routeHandler.getAllResultsHandler)
-                api.get("/results/:resultId", routeHandler.getResultHandler)
-                api.delete("/results/:resultId", routeHandler.deleteResultHandler)
-                api.patch("/results/:resultId", jsonBodyParser, routeHandler.updateResultHandler)
-                api.use(errorHandler)
-                api.listen(PORT, () => console.log(`API running on PORT ${PORT}`))
+
+        api.listen(PORT, () => {
+            console.log(`API running on PORT ${PORT}`)
+           
+            populateDatabase().catch(error => {
+                console.error("Error populating database:", error)
             })
+        })
+
+        api.get("/", (req, res) => res.send("Hello world"))
+        api.post("/users", jsonBodyParser, routeHandler.registerUserHandler)
+        api.post("/users/auth", jsonBodyParser, routeHandler.authenticateUserHandler)
+        api.get("/users/:targetUserId", routeHandler.getUsernameHandler)
+        api.get("/workouts/:workoutType", routeHandler.getRandomWorkoutHandler)
+        api.post("/posts", jsonBodyParser, routeHandler.createPostHandler)
+        api.get("/posts", routeHandler.getPostsHandler)
+        api.patch("/posts/:postId/likes", routeHandler.toggleLikePostHandler)
+        api.post("/posts/:postId/comments", jsonBodyParser, routeHandler.createCommentHandler)
+        api.get("/posts/:postId/comments", routeHandler.getAllCommentsHandler)
+        api.get("/results", routeHandler.getAllResultsHandler)
+        api.get("/results/:resultId", routeHandler.getResultHandler)
+        api.delete("/results/:resultId", routeHandler.deleteResultHandler)
+        api.patch("/results/:resultId", jsonBodyParser, routeHandler.updateResultHandler)
+        api.use(errorHandler)
+
+
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error("Error connecting to database:", error))

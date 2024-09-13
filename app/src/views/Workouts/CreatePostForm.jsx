2c0f70629076914a2e5a779d72648d9cce669d77
Debi312 <deboragarciaescudero@gmail.com>
2024-09-13 11:36:46 +0200

    update CSS for custom file input styling; replace default file input with a custom label

--- app/src/views/Workouts/CreatePostForm.jsx
+++ app/src/views/Workouts/CreatePostForm.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import { useState } from "react"
 import logic from "../../logic"
 import Button from "../../components/Button"
 import Field from "../../components/Field"
@@ -10,14 +10,16 @@ export default function CreatePostForm({ workoutId, onPostCreated }) {
     console.log("CreatePostForm -> render")
     const { alert } = useContext()
 
+    const [imageName, setImageName] = useState("")
+
     const handleCreatePostSubmit = (event) => {
-        
+
         event.preventDefault()
 
         const form = event.target
         const formData = new FormData()
 
-        formData.append('image', form.image.files[0]) // Aquí 'image' es el nombre del campo de archivo
+        formData.append('image', form.image.files[0])
         formData.append('description', form.description.value)
         formData.append('time', form.time.value && Number(form.time.value))
         formData.append('repetitions', form.repetitions.value && Number(form.repetitions.value))
@@ -25,11 +27,11 @@ export default function CreatePostForm({ workoutId, onPostCreated }) {
         formData.append('workoutId', workoutId)
         console.log("FormData contents:", Array.from(formData.entries()));
 
-        const image = formData.get('image') // Obtén el archivo
+        /* const image = formData.get('image') // Obtén el archivo
         const description = formData.get('description')
         const time = formData.get('time') && Number(formData.get('time'))
         const repetitions = formData.get('repetitions') && Number(formData.get('repetitions'))
-        const weight = formData.get('weight') && Number(formData.get('weight'))
+        const weight = formData.get('weight') && Number(formData.get('weight')) */
 
         /*  const image = form.image.value
          const description = form.description.value
@@ -50,12 +52,25 @@ export default function CreatePostForm({ workoutId, onPostCreated }) {
             console.error(error)
             alert(error.message)
         }
-
     }
+
+    const handleFileChange = (event) => {
+        setImageName(event.target.files[0].name)
+    }
+
     return (
         <form className="CreatePostForm" onSubmit={handleCreatePostSubmit}>
             <div className="post-form-container">
-                <Field id="image" type="file" placeholder="Upload image"></Field>
+                <label className="image-label" htmlFor="image-upload">
+                    <i className="fa-solid fa-camera"></i>{imageName || "Select file"}
+                </label>
+                <input
+                    id="image-upload"
+                    type="file"
+                    name="image"
+                    onChange={handleFileChange}
+                    style={{ display: 'none' }} // Oculta el input por defecto
+                />
                 <Field id="description" type="text" placeholder="Description"></Field>
                 <Field id="time" type="number" placeholder="Time"></Field>
                 <Field id="repetitions" type="number" placeholder="Total repetitions"></Field>
